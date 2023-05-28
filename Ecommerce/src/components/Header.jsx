45147e8fc7c845a4073189718d6e1c0e6b979cf2
Jhon Luna <jhonlunaa56@gmail.com>
2023-05-28 18:26:05 -0500

    ok

--- Ecommerce/src/components/Header.jsx
+++ Ecommerce/src/components/Header.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { Link } from 'react-router-dom'
-import mobiLogo from '../public/MOBI.png'
+//import mobiLogo from '../public/MOBI.png'
 import './Header.module.css'
 
 const Header = () => {
