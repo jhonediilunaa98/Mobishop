9baf50f4bf018e8da2e139f28a03ab8e58e29195
Jhon Luna <jhonlunaa56@gmail.com>
2023-05-28 22:13:14 -0500

    ok

--- Ecommerce/src/components/Home.jsx
+++ Ecommerce/src/components/Home.jsx
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import './Home.module.css'
 
 const Home = () => {
   return (
     <>
-      <h1>hola</h1>
+    <h1>hola</h1>
     </>
   )
 }
