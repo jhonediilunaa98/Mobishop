828ea2a1eea0e651c1e0736310251738430cfaf4
LuisaFCor <luisafernandaaa@gmail.com>
2023-05-29 13:47:30 -0500

    dzsdg

--- Ecommerce/src/components/Footer/Footer.jsx
+++ Ecommerce/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
  const Footer = () => {
     return ( 
         <footer>
-            <p>Todos los derechos reservados - Mobishop &copy;</p>
+            <p>Todos los derechos reservados - MobiShop &copy;</p>
         </footer>
      );
  }
