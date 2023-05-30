881a872167ffcdf32b9d5143a32b7bc4f49845d0
Jhon Luna <jhonlunaa56@gmail.com>
2023-05-30 17:49:17 -0500

    ok

--- Ecommerce/src/components/NewProduct.jsx
+++ Ecommerce/src/components/NewProduct.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
+import {DATA} from './Data'
 
 const NewProduct = () => {
 
-    const cardItem = (item) =>{
-        
+    const cardItem = (item) => {
+        <div className="product">
+        <img src={item.img} />
+        <div className="product-info">
+            <h4 className="product-title">{item.title}</h4>
+            <p className="product-price">{item.price}</p>
+            <a className="product-btn" href="#">Buy Now</a>
+        </div>
+    </div>
     }
-
+ 
     return (
-        <>
-            <div className="product">
-                <img src={airpod} />
-                <div className="product-info">
-                    <h4 className="product-title">AirPod 2nd Gen
-                    </h4>
-                    <p className="product-price">$129</p>
-                    <a className="product-btn" href="#">Buy Now</a>
-                </div>
+        <div>
+            <div>
+                {DATA.map(cardItem)}
             </div>
 
-
-            return(
-                <div>
-                    {DATA.map(cardItem)}
-                </div>
-            )
-        </>
+        </div>
     )
 }
 
