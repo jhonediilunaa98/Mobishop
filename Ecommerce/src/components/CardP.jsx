eff01f30ec12aafdaf55725cdc4cd85281c4ab02
Jhon Luna <jhonlunaa56@gmail.com>
2023-05-31 04:10:32 -0500

    ok

--- Ecommerce/src/components/CardP.jsx
+++ Ecommerce/src/components/CardP.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
-import Data from './Data.json'
+import { useState, useEffect } from 'react'
 
-function CardP(props) {
+
+
+
+
+const CardP = () => {
+  let [data, set] = useState([]);
+  useEffect(() => {
+    const obtener = () => {
+      return fetch(
+        'https://6476ec289233e82dd53a86ce.mockapi.io/api/v1/mobishop'
+      )
+        .then((res) => res.json())
+        .then((data) => set(data))
+        .catch((err) => console.error(err));
+    };
+    obtener();
+    return () => set([]);
+  }, []);
   return (
-    <>
-        <li>{props.el.title}</li>
-        
-    </>
-  )
+    <ul>
+      {data.map((item) => (
+        // eslint-disable-next-line react/jsx-key
+        <li>key = {item.id}<h2>{item.title}</h2><p>{item.price}</p><picture>{item.img}</picture></li>
+      ))}
+    </ul>
+  );
 }
-
+//<li>{props.el.title}</li>
 export default CardP
--- Ecommerce/src/components/Product.jsx
+++ Ecommerce/src/components/Product.jsx
@@ -1,26 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
+import CardP from './CardP'
 import './product.module.css'
-import Data from './Data.json'
-//import CardP from './CardP'
-import airpod from '../assets/AirPod 2nd Gen.jpg'
-import iphone from '../assets/iPhone-14.jpg'
-import apple from '../assets/apple-watch-pcq.jpg'
-import ipad from '../assets/ipad-pro.jpg'
 
 
 
-// function CardP(props) {
-//   return (
-//     <>
-//       <li>
-//         <h2>{props.el.title}</h2>
-//         <h2>{props.el.price}</h2>
-//       </li>
-      
-//     </>
-//   )
-// }
+
+
+
 
 
 const Product = () => {
@@ -36,64 +23,9 @@ const Product = () => {
         <button>Iphone 15</button>
         </article>
         <div className="all-products">
-          <div className="product">
-            <img src={airpod} />
-            <div className="product-info">
-              <h4 className="product-title">AirPod 2nd Gen
-              </h4>
-              <p className="product-price">$129</p>
-              <a className="product-btn" href="#">Buy Now</a>
-
-            </div>
-          </div>
-          <div className="product">
-            <img src={iphone} />
-            <div className="product-info">
-              <h4 className="product-title">Apple Watch
-              </h4>
-              <p className="product-price">$299</p>
-              <a className="product-btn" href="#">Buy Now</a>
-            </div>
-          </div>
-
-          
-          <div className="product">
-            <img src={apple} />
-            <div className="product-info">
-              <h4 className="product-title">iPhone 14
-              </h4>
-              <p className="product-price">$999</p>
-              <a className="product-btn" href="#">Buy Now</a>
-
-            </div>
-          </div>
-          <div className="product">
-            <img src={ipad} />
-            <div className="product-info">
-              <h4 className="product-title">iPan Pro
-              </h4>
-              <p className="product-price">$629*</p>
-              <a className="product-btn" href="#">Buy Now</a>
-            </div>
-          </div>
         </div>
-
       </section>
-
-
-      {/* <ul>
-        {Data.ProductosMobi.map((el, index) => (<CardP key={index} el={el} />))}
-      </ul> */}
-      
-         {/* <div className="product">
-            <img src={ipad} />
-            <div className="product-info">
-              <h4 className="product-title">iPan Pro
-              </h4>
-              <p className="product-price">$629*</p>
-              <a className="product-btn" href="#">Buy Now</a>
-            </div>
-          </div> */}
+      <CardP />
     </>
   )
 }
