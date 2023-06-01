efcbdc321dc64252ac29badcb75a9d8b8a3d2625
Michelle López <132721171+michelle27lopez@users.noreply.github.com>
2023-06-01 00:32:47 -0500

    skmksm

--- Ecommerce/src/components/Beneficios.jsx
+++ Ecommerce/src/components/Beneficios.jsx
@@ -13,37 +13,37 @@ function Beneficios() {
       <div className="ben-item">
         <img src={tienda} className="iconos-beneficios" alt="Compra Online Recoge en Tienda" />
         <div>
-          <h3 className="h15">Compra en línea y recoge en tienda en 1 hora</h3>
+          <h4 id='h155'>Compra en línea y recoge en tienda en 1 hora</h4>
         </div>
       </div>
       <div className="ben-item">
         <img src={horas} className="iconos-beneficios" alt="Aplica para Bogotá" />
         <div>
-          <h3>Aplica para Bogotá*</h3>
+        <h4 id='h155'>Aplica para Bogotá*</h4>
         </div>
       </div>
       <div className="ben-item">
         <img src={interes} className="iconos-beneficios" alt="0% de interés con Davivienda" />
         <div>
-          <h3 className="h15">0% de interés con Davivienda</h3>
+        <h4 id='h155'>0% de interés con Davivienda</h4>
         </div>
       </div>
       <div className="ben-item">
         <img src={addi} className="iconos-beneficios" alt="3 cuotas sin interés" />
         <div>
-          <h3 className="h15">3 cuotas sin interés</h3>
+        <h4 id='h155'>3 cuotas sin interés</h4>
         </div>
       </div>
       <div className="ben-item">
         <img src={garantia} className="iconos-beneficios" alt="1 año de garantía" />
         <div>
-          <h3 className="h15">1 año de garantía</h3>
+        <h4 id='h155'>1 año de garantía</h4>
         </div>
       </div>
       <div className="ben-item">
         <img src={tecnico} className="iconos-beneficios" alt="Servicio Técnico Certificado" />
         <div>
-          <h3 className="h15">Servicio Técnico Certificado</h3>
+        <h4 id='h155'>Servicio Técnico Certificado</h4>
         </div>
       </div>
     </div>
