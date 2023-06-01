9e3256bd087fa1874a6a02059a5ac1cb209e4b33
Michelle López <132721171+michelle27lopez@users.noreply.github.com>
2023-06-01 00:11:20 -0500

    dddd

--- Ecommerce/src/components/Beneficios.jsx
+++ Ecommerce/src/components/Beneficios.jsx
@@ -11,58 +11,40 @@ function Beneficios() {
   return (
     <div className="button-container">
       <div className="button-item">
-        <a href="#">
-        <img src={tienda}  className="iconos-beneficios" alt="Compra Online Recoge en Tienda" />
-          <div>
-            <h3 className="h15">Compra Online Recoge en Tienda en 1 hora</h3>
-          </div>
-        </a>
+        <img src={tienda} className="iconos-beneficios" alt="Compra Online Recoge en Tienda" />
+        <div>
+          <h3 className="h15">Compra en línea y recoge en tienda en 1 hora</h3>
+        </div>
       </div>
-
       <div className="button-item">
-        <a href="#">
-        <img src={horas}  className="iconos-beneficios" alt="Aplica para Bogotá" />
-          <div>
-            <h3>Aplica para Bogotá*</h3>
-          </div>
-        </a>
+        <img src={horas} className="iconos-beneficios" alt="Aplica para Bogotá" />
+        <div>
+          <h3>Aplica para Bogotá*</h3>
+        </div>
       </div>
-
       <div className="button-item">
-        <a href="#">
-        <img src={interes}  className="iconos-beneficios" alt="0% de interés con Davivienda" />
-          <div>
-            <h3 className="h15">0% de interés con Davivienda</h3>
-          </div>
-        </a>
+        <img src={interes} className="iconos-beneficios" alt="0% de interés con Davivienda" />
+        <div>
+          <h3 className="h15">0% de interés con Davivienda</h3>
+        </div>
       </div>
-
       <div className="button-item">
-        <a href="#">
         <img src={addi} className="iconos-beneficios" alt="3 cuotas sin interés" />
-          <div>
-            <h3 className="h15">3 cuotas sin interés</h3>
-          </div>
-        </a>
+        <div>
+          <h3 className="h15">3 cuotas sin interés</h3>
+        </div>
       </div>
-
       <div className="button-item">
-        <a href="#">
-            <h3 className="h15">1 año de garantía</h3>
-            <img src={garantia} className="iconos-beneficios" alt="1 año de garantía" />
-          <div>
-            <h3 className="h15">1 año de garantía</h3>
-          </div>
-        </a>
+        <img src={garantia} className="iconos-beneficios" alt="1 año de garantía" />
+        <div>
+          <h3 className="h15">1 año de garantía</h3>
+        </div>
       </div>
-
       <div className="button-item">
-        <a href="#">
         <img src={tecnico} className="iconos-beneficios" alt="Servicio Técnico Certificado" />
-          <div>
-            <h3 className="h15">Servicio Técnico Certificado</h3>
-          </div>
-        </a>
+        <div>
+          <h3 className="h15">Servicio Técnico Certificado</h3>
+        </div>
       </div>
     </div>
   );
