0c1de4f6a77bccffd81f5c139587ffd0207a0c1f
LuisaFCor <luisafernandaaa@gmail.com>
2023-05-31 20:38:06 -0500

    Login

--- Ecommerce/src/components/buttons/Nuevacuenta.jsx
+++ Ecommerce/src/components/buttons/Nuevacuenta.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react'
+import React, { useContext } from "react";
 import { Link } from 'react-router-dom'
+import UserContext from "../context/UserContext";
 
 const Nuevacuenta = () => {
-
-    const Usuarios = () => {
-        let [CrearCuenta ] = useState([]);
-        useEffect(() => {
-            const enviar = () => {
-                fetch('https://64769b539233e82dd53a2bb9.mockapi.io/registrer')
-                .then((respuesta) => resepuesta.json())
-                .then((data))
-            };
-        })
+  const { login, userError, user, userSuccess } = useContext(UserContext);
+  const enviar = (evento) => {
+    evento.preventDefault();
+    const { username, email, password } = evento.target.elements;
+    console.log(username.value, email.value, password.value);
+    login(username.value, email.value, password.value);
+    if (user) {
+      // redireccion
     }
+  };
 
     return ( <>
             <main className='main-form'>
@@ -24,21 +24,20 @@ const Nuevacuenta = () => {
                     <h1 className='title'>Crear Cuenta</h1>
                     <form 
                         id='form-login'
-                        onSubmit={CrearCuenta}
+                        action="#"
+                        onSubmit={enviar}
                     >
                         <input 
                             type="text"
-                            name='nombre'
+                            name='username'
                             placeholder='Tu Nombre'
                             id='email'
-                            value={nombre}
                          />
                         <input 
                             type="email"
                             name='email'
                             placeholder='Tu Email'
                             id='email'
-                            value={email}
                          />
                         
                         <input 
@@ -46,17 +45,19 @@ const Nuevacuenta = () => {
                             name='password'
                             placeholder='Tu Contraseña'
                             id='password'
-                            value={password}
                          />
 
                         <input 
                             type="submit"
                             className='btn-submit'
-                            value='Crear Cuenta'
                          />
                          <Link to={'/login'} className='new'>¿Ya tienes una cuenta?</Link>
                     </form>
-                    
+                    {userError ? (
+                    <p>{userError}</p>
+                    ) : userSuccess ? (
+                    <p>{userSuccess}</p>
+                    ) : null}
                 </section>
                 
             </main>        
--- Ecommerce/src/components/providers/UserProvider.jsx
+++ Ecommerce/src/components/providers/UserProvider.jsx
@@ -1,23 +1,92 @@
-import { useEffect, useState } from 'react'
-import UserContext from '../context/UserContext'
+import React, { useState } from "react";
+import UserContext from "../context/UserContext";
 
+const UserProvider = ({ children }) => {
+  const usuarios = [
+    { username: "", email: "", password: "" },
+  ];
+  const [user, setUser] = useState(null);
+  const [userError, setUserError] = useState(null);
+  const [userSuccess, setUserSuccess] = useState(null);
+  const login = (email, password) => {
+    fetch("https://64769b539233e82dd53a2bb9.mockapi.io/registrer", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: "Kevin", email: "coreo@gmail.com", password: "password"}, null, 3),
+    })
+      .then((respuesta) => respuesta.json())
+      .then((usuarios) => {
+        let encontrado = usuarios.find((usuario) => usuario.email == email);
+        if (!encontrado) {
+          setUser(null);
+          setUserError("usuario no encontrado");
+          setUserSuccess(null);
+          return false;
+        }
 
-const UserProvider = ({children}) => {
+        if (encontrado.password != password) {
+          setUser(null);
+          setUserError("clave no coincide");
+          setUserSuccess(null);
+          return false;
+        }
 
-    const [user, set] = useState(null);
-    const access = function(username, password){
-        fetch()
+        setUser(encontrado);
+        setUserError(null);
+        setUserSuccess("Bienvenido");
+        return true;
+      })
+      .catch((error) => {
+        setUser(null);
+        setUserError(error.msg);
+        setUserSuccess(null);
+        return error;
+      });
+  };
+
+  const loginAsync = async (email, password) => {
+    try {
+      const consulta = await fetch(
+        "https://646d4e359c677e232189e67c.mockapi.io/users"
+      );
+      const usuarios = await consulta.json();
+      let encontrado = usuarios.find((usuario) => usuario.email == email);
+      if (!encontrado) {
+        setUser(null);
+        setUserError("usuario no encontrado");
+        setUserSuccess(null);
+        return false;
+      }
+
+      if (encontrado.password != password) {
+        setUser(null);
+        setUserError("clave no coincide");
+        setUserSuccess(null);
+        return false;
+      }
+
+      setUser(encontrado);
+      setUserError(null);
+      setUserSuccess("Bienvenido");
+      return true;
+    } catch (error) {
+      setUser(null);
+      setUserError(error);
+      setUserSuccess(null);
+      return error;
     }
+  };
+  return (
+    <UserContext.Provider
+      value={{ user, userError, login, usuarios, userSuccess }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
 
-    const save = function(){
-        fetch()
-    }
-
-    return ( 
-           <UserContext.Provider value={{}}>{children}</UserContext.Provider>
-     );
-}
- 
 export default UserProvider;
 
 
