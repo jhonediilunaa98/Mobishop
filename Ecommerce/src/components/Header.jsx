0fe85e7e0b39053c401bda8363d2ef980c5d4fe3
Jhon Luna <jhonlunaa56@gmail.com>
2023-06-01 18:44:23 -0500

    modificacion del nav, heades y product

--- Ecommerce/src/components/Header.jsx
+++ Ecommerce/src/components/Header.jsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../public/Logo.svg';
 import './Header.module.css';
+import {FontAwesomeIcon} from '@Fortawesome/react-fontawesome';
+import {faSquareInstagram} from '@Fortawesome/free-brands-svg-icons';
+
 
 const Header = () => {
   const [isNavActive, setNavActive] = useState(false);
@@ -24,7 +27,11 @@ const Header = () => {
             {/* <Link to="/detal">Productos</Link>  */}
             <Link to="/about">Nosotros</Link>
             <Link to="/contact">Contacto</Link>
-         </ul>
+            </ul>
+            <div className='cartt'>
+                <FontAwesomeIcon  icon={faSquareInstagram} className='FontAwesomeIcon' />
+                <span className='spa itemm__total'>0</span>
+            </div>
         </nav>
         <div className="login">
           <Link to="/login">Sign in</Link>
