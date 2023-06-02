de1b60697898f45c97e36a0f5077ddbc856f51b6
Jhon Luna <jhonlunaa56@gmail.com>
2023-06-02 18:51:16 -0500

    ok

--- Ecommerce/src/components/Beneficios.jsx
+++ Ecommerce/src/components/Beneficios.jsx
@@ -9,44 +9,44 @@ import './BeneficiosMobi.module.css';
 
 function Beneficios() {
   return (
-    <section className="beneficio-container">
-      <figure className="ben-item">
+    <div className="beneficio-container">
+      <div className="ben-item">
         <img src={tienda} className="iconos-beneficios" alt="Compra Online Recoge en Tienda" />
-        <figure>
+        <div>
           <h4 id='h155'>Compra en línea y recoge en tienda en 1 hora</h4>
-        </figure>
-      </figure>
-      <figure className="ben-item">
+        </div>
+      </div>
+      <div className="ben-item">
         <img src={horas} className="iconos-beneficios" alt="Aplica para Bogotá" />
-        <figure>
+        <div>
         <h4 id='h155'>Aplica para Bogotá*</h4>
-        </figure>
-      </figure>
-      <figure className="ben-item">
+        </div>
+      </div>
+      <div className="ben-item">
         <img src={interes} className="iconos-beneficios" alt="0% de interés con Davivienda" />
-        <figure>
+        <div>
         <h4 id='h155'>0% de interés con Davivienda</h4>
-        </figure>
-      </figure>
-      <figure className="ben-item">
+        </div>
+      </div>
+      <div className="ben-item">
         <img src={addi} className="iconos-beneficios" alt="3 cuotas sin interés" />
-        <figure>
+        <div>
         <h4 id='h155'>3 cuotas sin interés</h4>
-        </figure>
-      </figure>
-      <figure className="ben-item">
+        </div>
+      </div>
+      <div className="ben-item">
         <img src={garantia} className="iconos-beneficios" alt="1 año de garantía" />
-        <figure>
+        <div>
         <h4 id='h155'>1 año de garantía</h4>
-        </figure>
-      </figure>
-      <figure className="ben-item">
+        </div>
+      </div>
+      <div className="ben-item">
         <img src={tecnico} className="iconos-beneficios" alt="Servicio Técnico Certificado" />
-        <figure>
+        <div>
         <h4 id='h155'>Servicio Técnico Certificado</h4>
-        </figure>
-      </figure>
-    </section>
+        </div>
+      </div>
+    </div>
   );
 }
 
