2b47e77d46934204326aea32ced6c82be5915310
LuisaFCor <luisafernandaaa@gmail.com>
2023-06-02 17:35:49 -0500

    ole

--- Ecommerce/src/components/Login.jsx
+++ Ecommerce/src/components/Login.jsx
@@ -16,10 +16,10 @@ const Login = () => {
             <main className='main-form'>
                 
                 <section className='section-1'>
-                    <h2></h2>
                 </section>
                 <section className='section-2'>
-                    <h1 className='title'>Iniciar Sesión</h1>
+                    <h1 className='title-form'>Iniciar Sesión</h1>
+                    <h2 className='subtitle-form'>Bienvenido/a </h2>
                     <p className='description-form'>Inicia Sesión para comprar</p>
                     <form 
                         id='form-login'
@@ -42,9 +42,13 @@ const Login = () => {
                         <input 
                             type="submit"
                             className='btn-submit'
+                            value='Ingresar'
                          />
                          {user 
-                         ? <Link to={'/'} className='Msg'>Ir a home</Link>
+                         ? <article className="alerta">
+                         <span className='Msg'>Bienvenido/a</span>
+                         <Link to={'/product'} className='Msg'>Ir a Productos </Link>
+                         </article>
                          : <Link to={'/nuevacuenta'} className='new'>¿No tienes una cuenta?</Link>
                          }
                          {userError 
