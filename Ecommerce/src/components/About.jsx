96e0c88cb924d195bc52f8cf1876b1266abeefd4
Michelle López <132721171+michelle27lopez@users.noreply.github.com>
2023-06-02 01:43:17 -0500

    jkk

--- Ecommerce/src/components/About.jsx
+++ Ecommerce/src/components/About.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import './About.module.css';
+import about from '../assets/about.png';
 
 const About = () => {
   return (
-    <>
-    </>
+    <section className="nosotros-section">
+      <div className="us-image-container">
+        <img className="us-image" src={about} alt="Nosotros" />
+      </div>
+      </section>
   )
 }
 
-export default About
+export default About;
